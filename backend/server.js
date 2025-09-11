d4e5b9074d623ce83c7e77731f27c3720ba5e3a9
Viktor Taseski <vtaseski24@gmail.com>
2025-09-11 17:54:19 +0200

    router with catch-all

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,12 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/tables", tableRoutes);
 app.use("/api/users", usersRouter);
 
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
