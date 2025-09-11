d52370639e94aad82804dd8212cf3703c343f2ea
Viktor Taseski <vtaseski24@gmail.com>
2025-09-11 17:56:30 +0200

    changed urls

--- backend/server.js
+++ backend/server.js
@@ -18,10 +18,5 @@ app.use("/api/users", usersRouter);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
-});
-
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -1,16 +1,13 @@
 // src/AppRoutes.js
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from "./App"; // customer-facing app
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./App";
 import WaiterLogin from "./components/WaiterLogin";
 
 function AppRoutes() {
     return (
         <Router>
             <Routes>
-                {/* Customer flow */}
                 <Route path="/*" element={<App />} />
-
-                {/* Waiter/Admin login */}
                 <Route path="/waiter-login" element={<WaiterLogin />} />
             </Routes>
         </Router>
