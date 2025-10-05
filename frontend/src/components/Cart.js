232f6baacf2154389f40f094efda080220fa9bda
Viktor Taseski <vtaseski24@gmail.com>
2025-10-05 15:50:38 +0200

    UI updates

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -381,7 +381,7 @@ function Cart({
 function Suggestions({ suggestions, qtyById, addToCart, removeFromCart }) {
     if (!suggestions.length) return null;
     return (
-        <div className="block" style={{ marginTop: "30px" }}>
+        <div className="block" style={{ marginTop: "10px" }}>
             <div className="block-title" style={{ textAlign: "center" }}>{t("cart.youMayLike")}</div>
             <ul className="menu-list menu-list--full">
                 {suggestions.map((item) => {
