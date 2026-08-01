4ef7f398ce686d2d09bb9ff2e32f73f957ec5232
Viktor Taseski <vtaseski24@gmail.com>
2026-08-01 22:57:27 +0200

    refactor: change from HashRouter to BrowseRouter

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Admin from './admin/Admin.js'
 
